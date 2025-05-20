0d8f6b5ba263dec23aab40074fe1c0b03eb8ff29
Dmitriy Tsybenko <cybenko2222@yandex.ru>
2025-05-20 21:31:54 +0300

    print_ni -> print_hi

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__name__':
-    print_ni('PyCharm')
+    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
