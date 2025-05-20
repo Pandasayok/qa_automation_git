11dcd3495722cd1147242ec5ffc87dccbeedb2a1
Dmitriy Tsybenko <cybenko2222@yandex.ru>
2025-05-20 21:13:07 +0300

    New massage

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press -R to execute is or replace it with your code.
+# Press Double o to search everywhere for classes file, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi,{name}') # Press xF8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__name__':
+    print_ni('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
